33042a14a5d1d796fc4ce5b6e2e3b10e7c2e8f30
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-02-15 11:58:06 +0530

    Feat:Developed the header component

--- Client/Tanahsarees/src/Components/MAIN/HEDAERS/Header/OptionsBar.jsx
+++ Client/Tanahsarees/src/Components/MAIN/HEDAERS/Header/OptionsBar.jsx
@@ -1,8 +1,10 @@
+import Search from "./Search";
+
 const OptionsBar = () => {
   return (
     <div>
-      {screen.width > 1000 && (
-        <div className="border-[#d5d5d5]  border-[0.15px] flex justify-center items-center w-[100vw] pl-15 gap-4 flex-wrap p-4 mt-7 darktext font-lato">
+      {(screen.width > 1000 && (
+        <div className="border-[#d5d5d5]  border-b-[0.15px] flex justify-center items-center w-[100vw] pl-15 gap-4 flex-wrap p-4 mt-7 darktext font-lato">
           {[
             "SALE",
             "SAREES",
@@ -21,6 +23,12 @@ const OptionsBar = () => {
             </span>
           ))}
         </div>
+      )) || (
+        <>
+          <div className="border-[#d5d5d5]  border-b-[0.15px] flex justify-center items-center w-[100vw]   flex-wrap pb-4  darktext font-lato">
+            <Search className="mt-[-1vmin]" />
+          </div>
+        </>
       )}
     </div>
   );
--- Client/Tanahsarees/src/Components/MAIN/HEDAERS/Header/Search.jsx
+++ Client/Tanahsarees/src/Components/MAIN/HEDAERS/Header/Search.jsx
@@ -1,14 +1,14 @@
 const Search = () => {
   return (
     <>
-      <div className="w-[80%] flex gap-x-3 border-[#d5d5d5] border-[0.25px] p-[0.35px] mt-4">
-        <a className="darktext text-[3.15vmin] ml-3">
+      <div className="w-[80%] flex gap-x-3 border-[#d5d5d5] border-[0.25px] p-[6px] lg:p-[0.35px] mt-4">
+        <a className="darktext text-[5vmin] lg:text-[3.15vmin] ml-3">
           <i className="ri-search-line"></i>
         </a>
         <input
           type="text"
           placeholder="Search For Items.."
-          className="darktext text-[2.15vmin] 2xl:text-[1.85vmin] pl-3 w-[250px] outline-none"
+          className="darktext text-[3.65vmin] lg:text-[1.85vmin] pl-1 lg:pl-3 w-[250px] outline-none"
         />
       </div>
     </>
