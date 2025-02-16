8bdf1f559031d27ce2ccafca562ae5f2a9dad5e8
anilrajput6441 <anilrajput6441@gmail.com>
2025-02-16 21:49:46 +0530

    FEAT:Developed Modal

--- Client/Tanahsarees/src/Components/MAIN/FEATURES/Feature.jsx
+++ Client/Tanahsarees/src/Components/MAIN/FEATURES/Feature.jsx
@@ -9,7 +9,7 @@ const Feature = () => {
   const data = [
     { url: "fast-delivery.png", text: "Fast Delivery" },
     { url: "quality.png", text: "Best Quality" },
-    { url: "secure-payment.png", text: "Secure Payment" },
+    { url: "credit-card.png", text: "Secure Payment" },
     { url: "price.png", text: "Best Price" },
     { url: "online-purchase.png", text: "100% Purchase protection" },
   ];
