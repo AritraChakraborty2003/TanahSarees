edcad20193b29868c925436f16a46da28d2295b2
anilrajput6441 <anilrajput6441@gmail.com>
2025-02-21 19:54:28 +0530

    FEAT:Developed the Cancel,Testimonial,Orders,Saree Models

---
+++ Backend/Models/Saree.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const sareeSchema = new mongoose.Schema(
+  {
+    sname: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 60,
+    },
+    type: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 100, // Removed trim (not needed for numbers)
+    },
+    photo: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    material: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    color: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    discount: {
+      type: Number,
+      min: 0,
+      max: 100,
+      default: 0, // Set default discount to 0
+    },
+    occasion: {
+      type: String,
+      trim: true,
+    },
+    topSelling: {
+      type: Boolean, // Changed from String to Boolean
+      default: false, // Default is false
+    },
+  },
+  {
+    collection: "Sarees", // Fixed incorrect syntax  this is collection name or table name
+    timestamps: true, // Fixed incorrect syntax
+  }
+);
+
+// export default mongoose.model("Saree", sareeSchema);
+
+const sareeObj = mongoose.model("Sarees", sareeSchema);
+export default sareeObj; //
---
+++ Backend/Models/Testimonials.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const testimonialSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 60,
+    },
+    review: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 1000,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    photo: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    collection: "testimonials",
+    timestamps: true,
+  }
+);
+
+const TestimonialsObj = mongoose.model("testimonials", testimonialSchema);
+
+export default TestimonialsObj;
