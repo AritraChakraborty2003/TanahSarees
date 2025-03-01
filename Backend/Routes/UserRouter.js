58b74e4528d0ec7ff2f001fe8e77c1cd84815a7d
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-03-02 01:18:46 +0530

    FEAT:On ADD REVIEW Or Buy now button check info updated or not

--- Backend/Routes/UserRouter.js
+++ Backend/Routes/UserRouter.js
@@ -2,9 +2,11 @@ import express from "express";
 import { UserGET } from "../Controllers/UserController/userGET.js";
 import { userDELETE } from "../Controllers/UserController/userDELETE.js";
 import { userPATCH } from "../Controllers/UserController/userPATCH.js";
+import { userCheckInfo } from "../Controllers/UserController/userCheckInfo.js";
 
 export const userRouter = express.Router();
 
 userRouter.get("/", UserGET);
+userRouter.get("/checkinfo/:id", userCheckInfo);
 userRouter.delete("/", userDELETE);
 userRouter.patch("/", userPATCH());
