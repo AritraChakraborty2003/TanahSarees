7f83449301b3972e3122533e45384ab57cd090f5
anilrajput6441 <anilrajput6441@gmail.com>
2025-03-06 20:11:29 +0530

    FEAT:CART and Payment page integration

--- Client/Tanahsarees/src/Components/CARDS/CartDisplay.jsx
+++ Client/Tanahsarees/src/Components/CARDS/CartDisplay.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import Tilt from "react-parallax-tilt";
+import { useContext } from "react";
+import { motion } from "framer-motion";
+import axios from "axios";
+import { AppContext } from "../../AppContext/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const CartDisplay = ({ data, chosen, setChosen }) => {
   const [isQuickView, setIsQuickView] = useState(false);
-  const { image, name, price, _id } = data;
+  const { photo, sname, price, _id, qty } = data;
+  const [count, setCount] = useState(qty);
+  const navigate = useNavigate();
+  const { activeProduct, setActiveProduct, newvar, setnewvar } =
+    useContext(AppContext);
 
   const handleCheckboxChange = (e) => {
     if (e.target.checked) {
@@ -14,12 +25,30 @@ const CartDisplay = ({ data, chosen, setChosen }) => {
     }
   };
 
+  const updateQuantity = async (productId, action) => {
+    console.log("pid:", productId);
+    try {
+      await axios.patch(
+        `${import.meta.env.VITE_APP_API_URL_TEST}api/v1/cart/${action}`,
+        { pid: productId },
+        { withCredentials: true }
+      );
+      action === "increase" ? setCount(count + 1) : setCount(count - 1);
+
+      setCartDrawerTigger(!cartDrawerTigger);
+      setnewvar(!newvar); // Trigger update in MainHeader
+    } catch (error) {
+      console.error("Failed to update cart", error);
+    }
+  };
+
   return (
     <div className="flex flex-col border-1 border-gray-200 rounded-lg shadow-lg">
       <div
         className="flex relative flex-col mb-4 w-[40vw] lg:w-[15vw] gap-y-2 lg:gap-y-1 cursor-pointer"
-        onMouseEnter={() => setIsQuickView(true)}
-        onMouseLeave={() => setIsQuickView(false)}
+
+        // onMouseEnter={() => setIsQuickView(true)}
+        // onMouseLeave={() => setIsQuickView(false)}
       >
         {/* Checkbox */}
         <div className="absolute z-50 top-2 left-2">
@@ -40,11 +69,23 @@ const CartDisplay = ({ data, chosen, setChosen }) => {
           transitionSpeed={500}
           className="rounded-lg"
         >
-          <img src={image} alt={name} />
+          <div
+            onClick={() => {
+              {
+                console.log("clicked");
+                setActiveProduct(data);
+                navigate("/product_descr");
+
+                window.scrollTo(0, 0);
+              }
+            }}
+          >
+            <img src={`${import.meta.env.VITE_APP_API_URL}` + photo} />
+          </div>
         </Tilt>
 
         {/* Quick View */}
-        <div
+        {/* <div
           className={`absolute w-[96%] text-[3vmin] lg:text-[1.85vmin] p-1 font-poppins bg-[#F58B75] text-amber-50 flex justify-center border-amber-50 bottom-[26.5%] lg:bottom-[22.3%] right-[1.9%] 
               transition-all duration-300 ease-in-out transform ${
                 isQuickView
@@ -53,12 +94,81 @@ const CartDisplay = ({ data, chosen, setChosen }) => {
               }`}
         >
           Quick View
-        </div>
+        </div> */}
 
         {/* Name & Price */}
         <div className="flex flex-col text-center text-gray-600">
-          <h2>{name}</h2>
-          <p>₹{price}</p>
+          <h2>{sname.slice(0, 22) + ".."}</h2>
+          <p className="font-semibold">₹{price}</p>
+          <div className="info w-[100%] h-[25%] flex items-center justify-center">
+            <div className="main w-[55%] mt-2 lg:mt-1 lg:w-[45%] ml-4 lg:ml-2 border-[#262424]  text-center border-1 h-[65%]  lg:h-[80%] flex items-center ">
+              <div
+                className="decrement border-r-1 border-[#262424] w-[25%] h-full flex justify-center items-center overflow-hidden "
+                id={`decr${_id}`}
+                onMouseEnter={() => {
+                  document.getElementById(`minus${_id}`).style.color =
+                    "#EEE5DA";
+                  document.getElementById(`decr${_id}`).style.backgroundColor =
+                    "#26242489";
+                }}
+                onMouseLeave={() => {
+                  document.getElementById(`minus${_id}`).style.color =
+                    "#262424";
+                  document.getElementById(`decr${_id}`).style.backgroundColor =
+                    "#fff";
+                }}
+                onClick={() => {
+                  count > 0
+                    ? (setCount(count - 1), updateQuantity(_id, "decrease"))
+                    : null;
+                }}
+              >
+                <p
+                  className="darktxt text-2xl cursor-pointer"
+                  id={`minus${_id}`}
+                >
+                  -
+                </p>
+              </div>
+              <div className="value  w-[50%] h-full flex justify-center items-center overflow-hidden ">
+                <motion.h1
+                  key={count}
+                  initial={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.45 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 10 }}
+                  className="text-md font-bold"
+                >
+                  {count}
+                </motion.h1>
+              </div>
+              <div
+                className="increment border-[#262424] border-l-1  w-[25%] h-full  flex justify-center items-center overflow-hidden"
+                id={`incr${_id}`}
+                onMouseEnter={() => {
+                  document.getElementById(`plus${_id}`).style.color = "#EEE5DA";
+                  document.getElementById(`incr${_id}`).style.backgroundColor =
+                    "#26242489";
+                }}
+                onMouseLeave={() => {
+                  document.getElementById(`plus${_id}`).style.color = "#262424";
+                  document.getElementById(`incr${_id}`).style.backgroundColor =
+                    "#fff";
+                }}
+                onClick={() => {
+                  setCount(count + 1);
+                  updateQuantity(_id, "increase");
+                }}
+              >
+                <p
+                  className="darktxt text-2xl cursor-pointer"
+                  id={`plus${_id}`}
+                >
+                  +
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
