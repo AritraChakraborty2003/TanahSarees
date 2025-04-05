061fd65fb3973127fca653c54fd52e7cf53dea15
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2025-04-05 19:05:55 +0530

    UPDATE:update in code

--- Backend/Routes/TestimonialRouter.js
+++ Backend/Routes/TestimonialRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { TestimonialsGET } from "../Controllers/TestimonialController/TestimonialGET.js";
 import { TestimonialPOST } from "../Controllers/TestimonialController/TestimonialPOST.js";
-import { ImageUploader } from "../utils/ImageUploader.js";
+import { ImageUploaderSingle } from "../utils/ImageUploaderSingle.js";
 import { ReviewPOST } from "../Controllers/ReviewControllers/ReviewPOST.js";
 
 export const TestimonialRouter = express.Router();
 TestimonialRouter.get("/", TestimonialsGET);
-TestimonialRouter.post("/", ImageUploader(), TestimonialPOST());
+TestimonialRouter.post("/", ImageUploaderSingle(), TestimonialPOST());
 TestimonialRouter.post("/reviews", ReviewPOST());
